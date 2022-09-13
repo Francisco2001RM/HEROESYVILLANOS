7fb39073eb5697a3487d98919b88a7975c54da05
Francisco2001RM <franrm014@gmail.com>
2022-09-13 15:38:48 +0200

    Ejemplo Instancia Objetos

---
+++ HEROESYVILLANOS/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+var spiderman = new Modelos.Heroe { Id = 1, Name = "", Surname = "", Power = "Araña" };
+Console.WriteLine(spiderman.Power);
+Modelos.Villano KingPin = new () { Id = 2, Name = String.Empty, Surname = "Fisk", Power=String.Empty };
+Modelos.Heroe Venom = new Modelos.Heroe();
+Console.WriteLine(KingPin.Surname);
+Console.WriteLine(Venom.Power);
+
+
+Console.WriteLine("Hello, World!");
